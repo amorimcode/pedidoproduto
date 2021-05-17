babe732caf3aec09aaf64f599c4e5640884dcb59
Bruno Amorim <bruno.amorim032@gmail.com>
2021-05-17 15:50:46 -0300

    tabela preços update

--- pedidoproduto.py
+++ pedidoproduto.py
@@ -1,8 +1,14 @@
 pedido = dict()
 pedidos = list()
+tabela_precos = dict()
+
+Tabela_Precos = {1000: ['Caneta Azul Pamper', 2], 1001: ['Estojo Canetinha Colorida', 5], 1002: ['Apagador de Quadro Negro', 7], 1003: [
+    'Caderno Sticker', 3], 1004: ['Mouse Pad Air', 25], 1005: ['Estojo Escolar', 10], 1006: ['Caderno 10 Volumes Musical', 8], 1007: ['Cola Bastão', 3], 1008: ["Borracha", 2], 1009: ['Fichário', 20], 1010: ['Grampeador', 10]}
 
 
 print("Digite o número do pedido: ")
+
+
 while True:
     pedidos.clear()
     Numero_Pedido = int(input("Digite número do pedido: "))
@@ -13,9 +19,10 @@ while True:
     QntB = int(input("Digite a quantidade: "))
     NumeroProdutoC = int(input("Digite número do produto: "))
     QntC = int(input("Digite a quantidade: "))
-    
-    pedido = {Numero_Pedido:[Numero_Produtos, {NumeroProdutoA:QntA,NumeroProdutoB:QntB,NumeroProdutoC:QntC}]}
-    
+
+    pedido = {Numero_Pedido: [Numero_Produtos, {
+        NumeroProdutoA: QntA, NumeroProdutoB: QntB, NumeroProdutoC: QntC}]}
+
     pedidos.append(pedido.copy())
     while True:
         resp = str(input('Quer Fazer mais pedido? [S/N] ')).upper()[0]
@@ -26,6 +33,4 @@ while True:
         break
 
 
-
 print(pedidos)
-
