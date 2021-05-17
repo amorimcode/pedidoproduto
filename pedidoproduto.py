3452969c1c8d0a5ad5320183126b694724f3f929
Bruno Amorim <bruno.amorim032@gmail.com>
2021-05-17 13:23:25 -0300

    cicero

--- pedidoproduto.py
+++ pedidoproduto.py
@@ -1,37 +1,31 @@
-
-print("===== Programa para fazer pedidos =====")
-print("Digite os pedidos separados por pedido.")
-print('')
+pedido = dict()
+pedidos = list()
 
 
-pedidonum = int(input("Digite o Número do Pedido: "))
-numproduct = int(input("Digite o Produto: "))
-qtdpedida = int(input("Digite a quantidade: "))
+print("Digite o número do pedido: ")
+while True:
+    pedidos.clear()
+    Numero_Pedido = int(input("Digite número do pedido: "))
+    Numero_Produtos = int(input("Digite número de Produtos que deseja: "))
+    NumeroProdutoA = int(input("Digite número do produto: "))
+    QntA = int(input("Digite a quantidade: "))
+    NumeroProdutoB = int(input("Digite número do produto: "))
+    QntB = int(input("Digite a quantidade: "))
+    NumeroProdutoC = int(input("Digite número do produto: "))
+    QntC = int(input("Digite a quantidade: "))
+    
+    pedido = {Numero_Pedido:[Numero_Produtos, {NumeroProdutoA:QntA,NumeroProdutoB:QntB,NumeroProdutoC:QntC}]}
+    
+    pedidos.append(pedido.copy())
+    while True:
+        resp = str(input('Quer Fazer mais pedido? [S/N] ')).upper()[0]
+        if resp in 'SN':
+            break
+        print("ERROR: RESPONDA (S) OU (N).")
+    if resp == 'N':
+        break
 
 
-# def Leitura_pedidos
-# N pedido (Int 4 Dig) +  N Produto (Int 3 dig) + Qnt pedida (Inteiro)
-# def leitura_pedidos ( PEDIDOS ) - função faz a leitura dos pedidos, organizando no dicionário – pode usar return se quiser
 
-pedido = {1: 'Caneta', 2: 'Lápis', 3: 'Caderno', 4: 'Tesoura', 5: 'Borracha',
-          6: 'Corretivo', 7: 'Apontador', 8: 'Cola', 9: 'Mochila', 10: 'Marca-texto'}
-print(pedido)
+print(pedidos)
 
-#  pedidos = {1}
-
-n_pedido = int(input("Digite o numero do pedido: "))
-quant_prod = int(input("Quantos produtos deste pedido?: "))
-i = 0
-
-while i < quant_prod:
-    n_produto = int(input("Nº Produto: "))
-    quantidade = int(input("Quantidade: "))
-    i += 1
-
-
-# def Imprime_relatório
-# def imprime_relatorio (Tab_Precos, PEDIDOS) - essa função deverá imprimir o relatório pedido;
-
-
-# def Valida_Produto
-# def valida_produto(CodigoProduto, Tab_Precos) – função que retorna True ou False se o código digitado no pedido existe ou não na Tabela de Preços;
